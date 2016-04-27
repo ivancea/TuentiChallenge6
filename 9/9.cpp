8a50f3a2acd2045d1486a99b7f9e2145380992b6
Iván Cea Fontenla <Ivn Cea Fontenla>
2016-04-27 14:44:20 +0200

    Backup: 27/04/2016, 14:44:19

---
+++ 9/9.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	cout << string("h")*5 << endl;
+}
---
+++ 9/doIt.bat
@@ -0,0 +1,4 @@
+python 9.py <sampleInput.txt >sampleOutput.txt
+REM python 9.py <testInput.txt >testOutput.txt
+python 9.py <submitInput.txt >submitOutput.txt
+pause
