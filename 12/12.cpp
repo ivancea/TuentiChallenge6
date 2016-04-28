f8abc41bf147a2f937fd8c8cb097103b8a93193a
ivancea <ivancea@mundo-r.com>
2016-04-28 21:26:47 +0200

    Backup: 28/04/2016, 21:26:47

--- 12/12.cpp
+++ 12/12.cpp
@@ -1,49 +1,89 @@
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
 
-int main(){
-    int c;
-	cin >> c;
-	for(int cc=1; cc<=c; cc++){
-		cerr << "Case #" << cc << ": ";
-		cout << "Case #" << cc << ": ";
-		uint64_t n,m,k;
-		cin >> n >> m >> k;
-		if((n*m)/m != n || n*m>k || k%m!=0){
-			cerr << "IMPOSSIBLE" << endl;
-			cout << "IMPOSSIBLE" << endl;
-		}else if(n*m==k){
-			cerr << '0' << endl;
-			cout << '0' << endl;
-		}else{
-			
-			uint64_t remaining = k-n*m;
-			int times = 0;
-			
-			uint64_t max = m;
-			uint64_t r = n-1;
-			
-			while(remaining > 0){
-				while(remaining>=max){ // TODO: while to equation. Sqrt?
-					remaining -= max;
-					max *= 2;
-					times++;
-				}
-				if(remaining == 0)
+struct City{
+	string name;
+	vector<City*> connections;
+	
+	bool operator<(const City& c){
+		return name<c.name;
+	}
+	bool operator==(const City& c){
+		return name==c.name;
+	}
+	bool operator!=(const City& c){
+		return name!=c.name;
+	}
+}
+
+void compare(set<City>& virusA, set<City>& virusB){
+	City* initialA = findInitial(virusA),
+		  initialB = findInitial(virusB);
+	
+	// Lets work!
+	
+}
+
+City* findInitial(const set<City>& cities){
+	for(const City& c1:cities){
+		bool good = true;
+		for(const City& c2:cities){
+			if(&c1==&c2)
+				continue;
+			for(City* c:c2.connections){
+				if(c==&c1){
+					good = false;
 					break;
-				
-				while(max>remaining)
-					max /= 2;
-				if(max!=m){
-					remaining -= max;
-					times++;
 				}
-				
 			}
-			cerr << times << endl;
-			cout << times << endl;
+			if(!good)
+				break;
 		}
+		if(good)
+			return &c1;
+	}
+	return nullptr;
+}
+
+int main(){
+	set<City> virusA;
+    int n;
+	cin >> n;
+	for(int i=0; i<n-1; i++){
+		string t;
+		
+		cin >> t;
+		City& s = virusA[t];
+		s->name = t;
+		
+		cin >> t;
+		City& d = virusA[t];
+		d->name = t;
+		
+		s.connections.push_back(&d);
+	}
+	
+	int c;
+	cin >> c;
+	for(int cc=0; cc<c; cc++){
+		set<City> virusB;
+		for(int i=0; i<n-1; i++){
+			string t;
+			
+			cin >> t;
+			City& s = virusB[t];
+			s->name = t;
+			
+			cin >> t;
+			City& d = virusB[t];
+			d->name = t;
+			
+			s.connections.push_back(&d);
+		}
+		cout << "Case #" + cc + ": ";
+		compare(virusA, virusB);
 	}
 }
